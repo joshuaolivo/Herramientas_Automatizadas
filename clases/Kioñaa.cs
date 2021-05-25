b83358fe1572511bfa2a403034a3211f739a0c9d
joshuaolivo <joshua.olivo.999@gmail.com>
2021-05-24 23:37:58 -0500

    Create Kioñaa.cs
    Prueba

---
+++ clases/Kioñaa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthyDiet.clases
+{
+    public class Kioñaa
+    {
+
+        public static void kioña()
+        {
+
+        }
+    }
+}
