834ac3f5c4ed3a904e3dec8511ed5764084fe62c
AntonReus <mhernandezn1602@alumno.ipn.mx>
2021-06-04 10:18:57 -0500

    Validación 2
    Le corregí la validación anterior

--- front/frmRegistro.aspx.cs
+++ front/frmRegistro.aspx.cs
@@ -42,15 +42,15 @@ namespace HealthyDiet.front
                 //LLAMAR HAS PASS
                 if (queys.Registrar(id, txtNombre.Value, txtApPaterno.Value, txtApMaterno.Value, Convert.ToString(ddlSexo.SelectedValue), txtEdad.Value, txtCorreo.Value, pass))
                 {
-                    if (txtCorreo.Value != txtCorreo2.Value & txtContraseña.Value != txtContraseña2.Value)
-                    {
-                        lblRespuesta.Text = "¡Revise la confirmación de correo y contraseña!";
-                        lblRespuesta.CssClass = "alert alert-warning m-1";
-                    }
-                    else
+                    if (txtCorreo.Value == txtCorreo2.Value && txtContraseña.Value == txtContraseña2.Value)
                     {
                         Session["idUser"] = queys.getId(txtCorreo.Value);
                         Response.Redirect("frmPlanMensual.aspx");
+                    }
+                    else
+                    {
+                        lblRespuesta.Text = "¡Revise la confirmación de correo y contraseña!";
+                        lblRespuesta.CssClass = "alert alert-warning m-1";
                     }
                 }
             }
